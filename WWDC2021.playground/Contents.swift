5a36d2ea5fba7eef2be3d9b70b01f5170761e93e
Federico Ciardi <fed.ciardi@gmail.com>
2021-03-09 16:41:18 +0100

    feat: add `MainScene`

---
+++ WWDC2021.playground/Contents.swift
@@ -0,0 +1,11 @@
+import PlaygroundSupport
+import SceneKit
+
+let sceneView = SCNView(frame: CGRect(x: 0 , y: 0, width: 640, height: 480))
+let scene = MainScene()
+sceneView.scene = scene
+
+PlaygroundPage.current.liveView = sceneView
+
+sceneView.showsStatistics = true
+sceneView.allowsCameraControl = true
